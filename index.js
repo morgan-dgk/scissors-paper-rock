ef536ddf52d93e6653e6c50676b5dcb8bf98cad2
Morgan Kerle <morgankerle@gmail.com>
2022-10-13 16:08:51 +1100

    Definitely not an atomic commit

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
 #!/usr/bin/env node
-const getPlayerInput = require("./lib/getPlayerInput");
+const getInput = require("./lib/utils");
+const play = require("./lib/play");
 
-getPlayerInput();
+async function main() {
+  let playerScore = 0;
+  let computerScore = 0;
+  for (let i = 0; i < 5; i++) {
+    let res = await play();
+    console.log(res);
+
+    switch (res) {
+      case 1:
+        playerScore += 1;
+        break;
+      case 0:
+        computerScore += 1;
+    }
+  }
+  console.log({ playerScore }, { computerScore });
+}
+
+main();
